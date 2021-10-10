ebdd84526206f8e29782fbda1c86997b911d61df
Qitain <475744133@qq.com>
2021-10-10 16:36:02 +0800

    Update dd_cookie.js
    优化qinglong2.9+兼容

--- dd_cookie.js
+++ dd_cookie.js
@@ -1,8 +1,12 @@
-
-let tokens = [
-
-     
-
-];
-
+let tokens = [];
+if (process.env.DD_TOKEN) {
+  if (process.env.DD_TOKEN.indexOf('&') > -1) {
+    tokens = process.env.DD_TOKEN.split('&');
+  } else if (process.env.DD_TOKEN.indexOf('\n') > -1) {
+    tokens = process.env.DD_TOKEN.split('\n');
+  } else {
+    tokens = [process.env.DD_TOKEN];
+  }
+}
+// console.log(tokens.length)
 module.exports = tokens;
